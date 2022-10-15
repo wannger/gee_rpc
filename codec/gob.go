907141987fa0280fc01c55de9f598e57838685a2
wannger <2363025390@qq.com>
2022-10-16 00:39:27 +0800

    111

---
+++ codec/gob.go
@@ -0,0 +1,29 @@
+package codec
+
+import (
+	"bufio"
+	"encoding/gob"
+	"io"
+)
+
+type GobCodec struct {
+	conn io.ReadWriteCloser
+	buf  *bufio.Writer
+	dec  *gob.Decoder
+	enc  *gob.Encoder
+}
+
+var _ Codec = (*GobCodec)(nil)
+
+func NewGodCodec(conn io.ReadWriteCloser) Codec {
+	buf := bufio.NewWriter(conn)
+	return &GobCodec{
+		conn: conn,
+		buf:  buf,
+		dec:  gob.NewDecoder(conn),
+		enc:  gob.NewEncoder(buf),
+	}
+}
+
+func (c *GobCodec) ReadHeader(h *Header) error {
+}
